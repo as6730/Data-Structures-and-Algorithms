179d12552c4c50a830f2c767c0508f6792485de8
Alexandra Savramis <alexandra.savramis@gmail.com>
2018-05-23 21:34:23 -0400

    Create two two_sum answers

---
+++ Interview_Prep.js
@@ -0,0 +1,42 @@
+// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// Given nums = [2, 7, 11, 15], target = 9,
+// Because nums[0] + nums[1] = 2 + 7 = 9,
+// return [0, 1].
+
+// Answer 1
+// Time Complexity: 0(n^2)
+// Space Complexity: O(1)
+
+let two_sum = function(nums, target) {
+    for (let i = 0; i < nums.length - 1; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) {
+                return [i, j];
+            }
+        }
+    }
+
+    return [];
+};
+
+// Answer 2
+// Time Complexity: 0(1) - traverse the list containing nn elements only once. Each look up in the table costs only O(1) time
+// Space Complexity: O(1)
+
+let two_sum = function(nums, target) {
+  let result = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    const val = target - nums[i];
+    const is_matched = nums.findIndex(el => el === val);
+    if (nums.includes(val) && is_matched) {
+      result = [i, is_matched];
+      break;
+    }
+  }
+
+  return result;
+}
+
+console.log(two_sum([2, 7, 11, 15], 9));
