8e8f0af2439267777b65c2943f1bd40f1156b7d0
Alexandra Savramis <alexandra.savramis@gmail.com>
2018-05-24 08:22:39 -0400

    Add func div by individual integers

--- Interview_Prep.js
+++ Interview_Prep.js
@@ -34,7 +34,7 @@ let two_sum = function(nums, target) {
     // this doesn't work because if the array given is [3, 3] and the target 6
     // then findIndex will just return the first index found where the condition
     // is true, and therefore it will never return true because of the duplicate
-    // numbers 
+    // numbers
     const is_matched = tmp_nums.findIndex(el => el === val);
     if (tmp_nums.includes(val) && is_matched) {
       result = [i, is_matched];
@@ -46,3 +46,56 @@ let two_sum = function(nums, target) {
 }
 
 console.log(two_sum([2, 7, 11, 15], 9));
+
+// Given the following, how would I group the item, so that I can get the result below
+
+var foodItems = [
+	{ type: "chinese", name: "chow mein" },
+	{ type: "american", name: "burgers" },
+	{ type: "chinese", name: "wontons" },
+  { type: "indian", name: "chicken" },
+];
+
+// iterate through the array
+
+let group_by_type = function(array) {
+  let result = [];
+
+  for (let i = 0; i < foodItems.length; i++) {
+
+  }
+}
+
+// result = [
+//   { type: "chinese", name: ["chow mein", "wontons"], count: 2 },
+//   { type: "america", name: ["burgers"], count: 1 },
+//   { type: "indian", name: "chicken", count: 1 }
+// ];
+
+// Given a range of numbers, see if it is divisible by its individual integers
+// ex: 128 - can be moduled by 1, 2, 8
+
+let divByComponents = function(lowerbound, upperbound) {
+  let result = [];
+
+  for (let i = lowerbound; i <= upperbound; i++) {
+    if (integers(i)) {
+      result.push(i);
+    }
+  }
+
+  return result;
+}
+
+let integers = function(num) {
+  let arr = num.toString().split("");
+  console.log(arr)
+
+  for (let i = 0; i < arr.length; i++) {
+    if (num % parseInt(arr[i]) != 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
