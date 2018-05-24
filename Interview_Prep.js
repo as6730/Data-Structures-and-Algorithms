c64594dd8530fa62b1a019089bfd22ec42bdbd80
Alexandra Savramis <alexandra.savramis@gmail.com>
2018-05-24 11:19:37 -0400

    Solve removeDuplicatesInPlace

--- Interview_Prep.js
+++ Interview_Prep.js
@@ -188,6 +188,38 @@ let isValid = function(s) {
 
 console.log(inputStringValid("{{([])}"));
 
+// Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+
+// Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+// Example 1:
+
+// Given nums = [1,1,2],
+
+// Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+
+// It doesn't matter what you leave beyond the returned length.
+// Example 2:
+
+// Given nums = [0,0,1,1,1,2,2,3,3,4],
+
+// Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively.
+
+// It doesn't matter what values are set beyond the returned length.
+
+let removeDuplicatesInPlace = function(array) {
+  for (let i = 0; i < array.length - 1; i++) {
+    if (array[i] === array[i + 1]) {
+      array.splice(i, 1);
+      i--;
+    }
+  }
+
+  return array.length;
+}
+
+console.log(removeDuplicatesInPlace([0,0,1,1,1,2,2,3,3,4]))
+
 // •	Given a dictionary = {'e': 5, 'a': 7, 'c': 3, 'd': 10, 'b': 8},
 // print out the keys of the top 3 values
 
